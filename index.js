ff10108cf1eaec5b980b794de5e49a62a6a728fc
first try <mercytadesse40@gmail.com>
2024-12-07 09:49:13 +0300

    connected the code with API to give the really temperature of the city

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+  let apiKey = `8oed3366e72ca42et0cf2f4b44f179e7`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let cityName = document.querySelector("#city");
   cityName.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchSubmit);
+searchCity("Addis Ababa");
